b6114e40386d4cd78f063b0f02c4edc080252298
unknown <farhahtahir@gmail.com>
2019-10-16 22:29:37 +0800

    parking application

--- app/Http/Controllers/ParkingController.php
+++ app/Http/Controllers/ParkingController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RoomController extends Controller
+class ParkingController extends Controller
 {
     //
     public function __construct()
