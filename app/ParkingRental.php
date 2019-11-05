bd2e60c70e5c6a73475114697a24b3cbcdc194b7
unknown <farhahtahir@gmail.com>
2019-11-05 23:09:34 +0800

    parking rental

---
+++ app/ParkingRental.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ParkingRental extends Model
+{
+    //
+    protected $table = 'parking_record';
+}
+
--- resources/views/parking_application.blade.php
+++ resources/views/parking_application.blade.php
@@ -12,38 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="matrix" class="col-md-4 col-form-label text-md-right">{{ __('No. Matrik:') }}</label>
+                            <label for="plat" class="col-md-4 col-form-label text-md-right">{{ __('No. Receipt:') }}</label>
 
                             <div class="col-md-6">
-                                <input type="text" name="matrix" id="matrix" required autofocus class="form-control">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nama:') }}</label>
-
-                            <div class="col-md-6">
-                                <input class="form-control" type="text" name="name" id="name" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="bilik" class="col-md-4 col-form-label text-md-right">{{ __('No. Bilik:') }}</label>
-
-                            <div class="col-md-6">
-                                <input class="form-control" type="bilik" name="bilik" id="bilik" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="sesi" class="col-md-4 col-form-label tex-md-right">{{ __('Sesi:') }}</label>
-
-                            <div class="col-md-6">
-                                <select name="Sesi" id="floor" class="form-control">
-                                    <option value="">{{ __('Pilih Sesi') }}</option>
-                                    <option value="1">{{ __('2017/2018') }}</option>
-                                    <option value="2">{{ __('2018/2019') }}</option>
-                                </select>
+                                <input class="form-control" type="text" name="plat" id="plat" required>
                             </div>
                         </div>
 
@@ -70,22 +42,6 @@
                                 <input class="form-control" type="text" name="warna" id="warna" required>
                             </div>
                         </div>
-
-
-                        <div class="form-group row">
-                            <label for="kategori" class="col-md-4 col-form-label tex-md-right">{{ __('Jenis CC kereta:') }}</label>
-
-                            <div class="col-md-6">
-                                <select name="kategori" id="kategori" class="form-control">
-                                    <option value="">{{ __('Pilih CC') }}</option>
-                                    <option value="A">{{ __('1.0') }}</option>
-                                    <option value="B">{{ __('1.3') }}</option>
-                                    <option value="B">{{ __('1.5') }}</option>
-                                    <option value="B">{{ __('2.0') }}</option>
-                                </select>
-                            </div>
-                        </div>
-
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
