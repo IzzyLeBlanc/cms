78ac427629c3ef5cbd8ed75fef737cca375e984e
unknown <farhahtahir@gmail.com>
2019-11-07 00:59:44 +0800

    add

--- resources/views/parking_application.blade.php
+++ resources/views/parking_application.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col=md-8">
             <div class="card">
-                <div class="card-header">{{ __('PENAMBAHAN TEMPAT LETAK KERETA') }}</div>
+                <div class="card-header">{{ __('PERMOHONAN TEMPAT LETAK KERETA') }}</div>
                 
                 <div class="card-body">
                     <form method="POST" action="{{route('create-parking-rental')}}" enctype="multipart/form-data">
@@ -69,44 +69,17 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" name="create" id="create" formaction="{{route('create-parking')}}">
-                                    {{ __('Hantar') }}
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary" name="create" id="create">
+                                        {{ __('Hantar') }}
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-                <table class=" table table-striped" id="table">
-                    <thead>
-                        <tr>
-                            <th>No Matrik</th>
-                            <th>No.Lot Parking</th>
-                            <th>No. Plat Kenderaan</th>
-                            <th>Jenis Kenderaan</th>
-                            <th>Warna Kenderaan</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($parkingapp as $parkingapp)
-                            <tr>
-                                 <td>{{$parkingapp->studentid}}</td>
-                                 <td>{{$parkingapp->parkingid}}</td>
-                                 <td>{{$parkingapp->receiptNo}}</td>
-                                 <td>{{$parkingapp->plateNo}}</td>
-                                 <td>{{$parkingapp->carModel}}</td>
-                                 <td>{{$parkingapp->carColor}}</td>
-                                 <td>{{$parkingapp->status}}</td>
-                                 <td>
-                                   <a href="{{route('update-parking', $parkingapp->id)}}" class="btn btn-danger">{{__('Edit')}}</a>
-                                 </td>
-         
-                            </tr>
-                        @endforeach
-                         </tbody>
-                      </table>
-                     {{$parkingapp->links() }}
-                 </div>
-             </div>
-         </div>
-         @endsection
+        </div>
+    </div>
+</div>
+@endsection
