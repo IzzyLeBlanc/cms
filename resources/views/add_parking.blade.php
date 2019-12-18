05d5c7ed2ed9aba93656b70eb4f662aa39ac093b
unknown <farhahtahir@gmail.com>
2019-12-18 16:11:13 +0800

    add

--- resources/views/add_parking.blade.php
+++ resources/views/add_parking.blade.php
@@ -40,8 +40,9 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" name="create" id="create" formaction="{{route('create-parking')}}">
-                                    {{ __('Hantar') }}
+                                    <button type="submit" class="btn btn-primary" name="create" id="create">
+                                            {{ __('Hantar') }}
+                                        </button>
                             </div>
                         </div>
                     </form>
